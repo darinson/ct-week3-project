1a274bbebca012feef0df2e9390ef0adec42d9a6
Darin Son <dson64@outlook.com>
2021-04-02 12:22:22 -0700

    Created dictionaries for inputs, restructured how the calculator receives input and fills the dictionaries. -DS

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 class roiCalc():
 
     def __init__(self):
-        print("Hello, welcome to your return-on-investment (ROI) calculator.\nWe will calculate your cash on cash ROI by collecting some information.")
-        print("Part 1/4: General Information")
+        print("\nHello, welcome to your return-on-investment (ROI) calculator.\nWe will calculate your cash on cash ROI by collecting some information.")
+        print("\nPart 1/4: General Information")
         self.Name = input("What is your property name? ")
         # property taxes use assessed value
-        self.PropertyValue = input("What is the value of your property? ")
+        self.PropertyValue = input("What is the value of your property? $")
 
         print("\nPart 2/4: Initial Investment Costs")
         self.Investment = self.cal_investment()
@@ -24,77 +24,119 @@ class roiCalc():
 
     def cal_income(self):
         dict_income = {
-            "rent": 0,
-            "laundry": 0,
-            "storage": 0,
-            "parking": 0,
-            "pet-rent": 0,
-            "miscellaneous": 0
+            "Rent": 0
         }
-        dict_income["rent"] = input("How much rent do you collect each month?")
-        dict_income["laundry"] = input(
-            "How much laundry fees do you collect each month?")
-        dict_income["storage"] = input(
-            "How much storage fess do you collect each month?")
-        dict_income["parking"] = input(
-            "How much parking fees do you collect each month?")
-        dict_income["pet-rent"] = input(
-            "How much pet-rent do you collect each month?")
-        dict_income["miscellaneous"] = input(
-            "How much miscellaneous fees do you collect each month?")
+        print("Monthly Income = money you earn from the property every month.")
+        print("This includes rent from your tenants.")
+        other_inputs = input(
+            "Are there other sources of income you would like to include? (y/n) ")
+        while other_inputs.lower() != "n":
+            if other_inputs != "y":
+                other_inputs = input(
+                    "Are there other sources of income you would like to include? (y/n) ")
+            else:
+                new_key = input(
+                    "What source of income would you like to include? ")
+                dict_income[new_key] = 0
+                other_inputs = input(
+                    "Would you like to add another source of income? (y/n) ")
+
+        for key in dict_income:
+            ask_value = input(
+                f"Please enter your monthly earnings from {key}: $")
+            while not ask_value.isdigit():
+                ask_value = input(f"Please enter a valid value of {key}: $")
+            dict_income[key] = ask_value
+
         askIncome = sum(int(costs) for x, costs in dict_income.items())
         return askIncome
 
     def cal_expenses(self):
         dict_expenses = {
-            "tax": 0,
-            "insurance": 0,
-            "utilities": 0,
-            "HOA": 0,
-            "vacancy": 0,
-            "miscellaneous": 0
+            "Property Tax Rate": 0,
+            "Insurance Rate": 0,
+            "Utilities": 0,
+            "HOA Fees": 0,
+            "Vacancy": 0
         }
-        dict_expenses["tax"] = input(
-            "What is the tax rate for your property in %? (Applied to home value)")
-        dict_expenses["insurance"] = input(
-            "How much do you pay per month for insurance? ")
-        dict_expenses["utilities"] = input(
-            "How much do you pay per month for utilities? ")
-        dict_expenses["HOA"] = input(
-            "How much do you pay per month for HOA fees? ")
-        dict_expenses["vacancy"] = input(
-            "How much do you save aside per month for vacancy?")
-        dict_expenses["miscellaneous"] = input(
-            "What other monthly costs are there? ")
+        print("Monthly Expenses = what you pay for the property every month.")
+        print("This includes property tax, insurance, utility bills, HOA fees, vacancy.")
+        other_inputs = input(
+            "Are there other types of expenses you would like to include? (y/n) ")
+        while other_inputs.lower() != "n":
+            if other_inputs != "y":
+                other_inputs = input(
+                    "Are there other types of expenses you would like to include? (y/n) ")
+            else:
+                new_key = input(
+                    "What type of expense would you like to include? ")
+                dict_expenses[new_key] = 0
+                other_inputs = input(
+                    "Would you like to add another type of expense? (y/n) ")
+
+        for key in dict_expenses:
+            if "rate" in key.lower():
+                ask_value = input(f"Please enter the monthly {key}:" + "%")
+                ask_value *= self.PropertyValue
+            else:
+                ask_value = input(
+                    f"Please enter your monthly earnings from {key}: $")
+            while not ask_value.isdigit():
+                ask_value = input(
+                    f"Please enter a valid value for monthly {key}: $")
+            dict_expenses[key] = ask_value
+
         askExpenses = sum(int(costs) for x, costs in dict_expenses.items())
         return askExpenses
 
     def cal_cashflow(self):
         askCashFlow = self.Income - self.Expenses
-        print("Your monthly cash flow is {}".format(str(askCashFlow)))
+        print("Your monthly cash flow is ${}.".format(str(askCashFlow)))
         return askCashFlow
 
     def cal_investment(self):
         dict_inv = {
-            "advancepayment": 0,
-            "closingcost": 0,
-            "repairs": 0,
-            "miscellaneous": 0
+            "Down Payment": 0,
+            "Repairs": 0,
+            "Closing Costs": 0
         }
-        dict_inv["advancepayment"] = input(
-            "How much did you pay for in advance? (i.e. downpayment) ")
-        dict_inv["closingcost"] = input("What are your closing costs? ")
-        dict_inv["repairs"] = input(
-            "How much did you spend on repairing the house when you first bought it? ")
-        dict_inv["miscellaneous"] = input(
-            "What other costs were there when you initially bought the house? ")
-        askInvestment = sum(int(costs) for x, costs in dict_inv.items())
+        print("Initial investment costs = what you paid before and when you bought the property.")
+        print("This includes down payments, repairs and closing costs.")
+        other_inputs = input(
+            "Are there other types of investment costs you would like to include? (y/n) ")
+        while other_inputs.lower() != "n":
+            if other_inputs != "y":
+                other_inputs = input(
+                    "Are there other types of investment costs you would like to include? (y/n) ")
+            else:
+                new_key = input(
+                    "What type of investment cost would you like to include? ")
+                dict_inv[new_key] = 0
+                other_inputs = input(
+                    "Would you like to add another type of investment cost? (y/n) ")
+
+        for key in dict_inv:
+            ask_value = input(f"Please enter the value of {key}: $")
+            while not ask_value.isdigit():
+                ask_value = input(f"Please enter valid value of {key}: $")
+            dict_inv[key] = ask_value
+
+        askInvestment = sum(float(costs) for x, costs in dict_inv.items())
         return askInvestment
 
     def cal_roi(self):
         annual_cashflow = 12 * self.Cashflow
         askROI = 100 * annual_cashflow / self.Investment
-        print("Your cash on cash ROI is {}%.".format(str(askROI)))
+        print("Your cash on cash ROI is {:.2f}%.".format(str(askROI)))
+        summary = input(
+            "\nWould you like a summary of the calculations? (y/n)")
+        while summary.lower() != "n":
+            summary = "n"
+            if summary.lower() != "y":
+                summary = input(
+                    "\nWould you like a summary of the calculations? (y/n)")
+            else:
+                pass
         return askROI
 
 
